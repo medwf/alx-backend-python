c4e5eaaf604f8447fdbddc5013b361b7013f83ac
med.wf <med.wf95@gmail.com>
2024-04-18 18:23:04 +0100

    add Function concat with annotations - task 1 😎

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+function:
+    concat is a function that take to string.
+        and return a concat string.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    method:
+        Concatenates two strings.
+    return:
+        string that concat two string.
+    '''
+    return str1 + str2
