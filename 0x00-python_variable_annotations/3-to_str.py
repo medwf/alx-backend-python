c9c19c9b48af2f0116b6bcbae1dff1ca2a4b2f7a
med.wf <med.wf95@gmail.com>
2024-04-18 18:53:46 +0100

    add to_str that return str of float - task 3 👍

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+function:
+    to_str: is function that cast an float to str.
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    method:
+        Converts a floating-point number to a string.
+    return:
+        a string of that float.
+    '''
+    return str(n)
