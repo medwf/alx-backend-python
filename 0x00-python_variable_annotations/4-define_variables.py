b591d0b84b9b22e08580cc21f500ae6e95aca6d7
med.wf <med.wf95@gmail.com>
2024-04-18 19:05:35 +0100

    define multitype variable with type annotations - task 4 ✌️

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+variable:
+    a: is a integer that take 1 as value.
+    pi: is a float that take 3.14 as value.
+    i_understand_annotations: is a boolean that take True as value.
+    school: is a string that take Holberton as value.
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
