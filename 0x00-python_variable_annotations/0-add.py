3961210c085c3fa7770cbc39b8c746cbadbd585d
med.wf <med.wf95@gmail.com>
2024-04-18 18:16:18 +0100

    add new projrcts python annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+function:
+    add with python annotations
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    this method should:
+        Adds two floating-point numbers.
+    return:
+        this methods should return float number.
+    '''
+    return a + b
