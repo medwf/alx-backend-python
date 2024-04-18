c922b26bf45b1ee04788cd033bf9d49f8eaa69a5
med.wf <med.wf95@gmail.com>
2024-04-18 18:37:15 +0100

    adding function floor - taht take a float and return an integer 🫡  - task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+Function:
+    floor that take a float.
+    return an integer of that float
+'''
+
+
+def floor(a: float) -> int:
+    '''
+    method:
+        Computes the floor of a floating-point number.
+    return:
+        an integer of that float.
+    '''
+    return int(a)
