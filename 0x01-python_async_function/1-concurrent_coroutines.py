9d4ca601219cf0f5fa257f8150d51f84d602cee0
med.wf <med.wf95@gmail.com>
2024-04-22 13:10:13 +0100

    execute multi methods at sametimes - task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from asyncio import gather
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+"""
+module for task 1.
+"""
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    spawn wait_random n times with the specified max_delay.
+    Args:
+        n: <int> repeat max_delay.
+        max_delay: <int> max delay.
+    return:
+        <list[float]>: list of all the delays (float values) sorted.
+    """
+    return sorted(
+        await gather(
+            *list(
+                map(lambda _: wait_random(max_delay), range(n))
+            )
+        )
+    )
