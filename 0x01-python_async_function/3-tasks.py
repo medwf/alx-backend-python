cdbba205901eb8536bc97c0d774783fcb17aaa40
med.wf <med.wf95@gmail.com>
2024-04-22 14:19:05 +0100

    add task 3 - create task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module for task 3
+"""
+from asyncio import create_task
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """
+    DOC
+    """
+    return create_task(wait_random(max_delay))
