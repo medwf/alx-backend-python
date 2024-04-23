7472a5d24fc61eb0a34dd009ce1e7f8167259605
med.wf <med.wf95@gmail.com>
2024-04-23 15:47:49 +0100

    add task 1 - messing annotations sorry 😅

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,9 +4,10 @@ module for task 1
 """
 
 async_generator = __import__('0-async_generator').async_generator
+from typing import List
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     coroutine will collect 10 random numbers
         using an async comprehension over async_generator
