6583fafec06a40a067ca632ffec39531737c6620
med.wf <med.wf95@gmail.com>
2024-04-23 15:49:30 +0100

    add task 1 - error pycodestyle sorry 😅

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,8 +3,8 @@
 module for task 1
 """
 
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
