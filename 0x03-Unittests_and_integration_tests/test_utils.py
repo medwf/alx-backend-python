8b7490981602b390d1e87a7381de98aa51f48371
med.wf <med.wf95@gmail.com>
2024-05-24 15:27:02 +0100

    add task 1, 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,9 @@
 module for test utils
 """
 import unittest
-from utils import access_nested_map
+from typing import Dict
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 
 
@@ -18,3 +20,41 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """test access_nested_map methods"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a", ), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """test access nested nap exception"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """TestGetJson define"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url: str, test_payload: Dict):
+        """Test get_json method"""
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
+
+# class TestMemoize(unittest.TestCase):
+#     """TestMemoize define"""
+
+#     def test_memoize(self):
+#         """test memoize methods"""
+#         class TestClass:
+
+#         def a_method(self):
+#             return 42
+
+#         @memoize
+#         def a_property(self):
+#             return self.a_method()
