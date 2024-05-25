44e1cdba23174c6d481367ed3bfeaf895b6a0ccf
med.wf <med.wf95@gmail.com>
2024-05-25 15:56:40 +0100

    test 2

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
-"""A module for testing the client module."""
+"""A module for testing the client module.
+"""
 import unittest
 from typing import Dict
-from unittest.mock import patch, MagicMock
+from unittest.mock import (
+    MagicMock,
+    patch,
+)
 from parameterized import parameterized
-from client import GithubOrgClient
 
+from client import (
+    GithubOrgClient
+)
 
 class TestGithubOrgClient(unittest.TestCase):
-    """Tests the `GithubOrgClient` class."""
+    """
+    TestGithubOrgClient define
+        For testing class GithubOrgClient
+    """
+
     @parameterized.expand([
         ("google", {'login': "google"}),
         ("abc", {'login': "abc"}),
@@ -16,11 +26,11 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch(
         "client.get_json",
     )
-    def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
-        """Tests the `org` method."""
-        mocked_fxn.return_value = MagicMock(return_value=resp)
-        gh_org_client = GithubOrgClient(org)
-        self.assertEqual(gh_org_client.org(), resp)
-        mocked_fxn.assert_called_once_with(
+    def test_org(self, org: str, responce: Dict, moked: MagicMock) -> None:
+        """Tests the org method using Mock and parameterized"""
+        moked.return_value = MagicMock(return_value=responce)
+        instance = GithubOrgClient(org)
+        self.assertEqual(instance.org(), responce)
+        moked.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
