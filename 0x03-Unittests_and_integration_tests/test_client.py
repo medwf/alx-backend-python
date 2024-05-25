0c2084e0966eb5770634976b31dfe6b52cbc1f8d
med.wf <med.wf95@gmail.com>
2024-05-25 15:27:09 +0100

    finally 😲

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""test client module"""
+import unittest
+from unittest.mock import patch, Mock, MagicMock
+from parameterized import parameterized
+from typing import Dict
+from client import GithubOrgClient
+
+class TestGithubOrgClient(unittest.TestCase):
+    """TestGithubOrgClient define"""
+
+    @parameterized.expand([
+        ('google', {'api': 'google'}),
+        ('abc', {'api': 'abc'}),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org: str, responce: Dict, moked: MagicMock) -> None:
+        """test client"""
+        moked.return_value = MagicMock(return_value=responce)
+        instance = GithubOrgClient(org)
+        self.assertEqual(instance.org(), responce)
+        moked.assert_called_once()
+
