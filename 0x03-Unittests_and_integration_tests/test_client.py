7e9c39096541be531cb9bf410a62d40c7b5805d9
med.wf <med.wf95@gmail.com>
2024-05-25 18:46:04 +0100

    add task 5 - mocked property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,12 +6,14 @@ from typing import Dict
 from unittest.mock import (
     MagicMock,
     patch,
+    PropertyMock,
 )
 from parameterized import parameterized
 
 from client import (
     GithubOrgClient
 )
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """
@@ -34,3 +36,19 @@ class TestGithubOrgClient(unittest.TestCase):
         moked.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
+
+    def test_public_repos_url(self) -> None:
+        """Tests the _public_repos_url method"""
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+                ) as mockProp:
+
+            mockProp.return_value = {
+                "repos_url": "https://api.github.com/orgs/google/repos"
+                }
+
+            self.assertEqual(
+                GithubOrgClient('google')._public_repos_url,
+                "https://api.github.com/orgs/google/repos"
+                )
