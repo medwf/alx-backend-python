a2c2f73b7bb0f71f44387ac419a9ea9ca820ba77
med.wf <med.wf95@gmail.com>
2024-05-26 21:09:19 +0100

    fix test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -53,71 +53,24 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://api.github.com/orgs/google/repos"
             )
 
-    @patch('client.get_json')
+    @patch('client.get_json',
+           return_value=[{'name': 'abc'}, {'name': 'defg'}]
+           )
     def test_public_repos(self, moked_get_json: MagicMock) -> None:
         """tests the public repos method"""
-        test_payload = {
-            'repos_url': "https://api.github.com/users/google/repos",
-            'repos': [
-                {
-                    "id": 7697149,
-                    "node_id": "MDEwOlJlcG9zaXRvcnk3Njk3MTQ5",
-                    "name": "episodes.dart",
-                    "full_name": "google/episodes.dart",
-                    "private": False,
-                    "license": {
-                        "key": "bsd-3-clause",
-                        "name": "BSD 3-Clause \"New\" or \"Revised\" License",
-                        "spdx_id": "BSD-3-Clause",
-                        "url": "https://api.github.com/licenses/bsd-3-clause",
-                        "node_id": "MDc6TGljZW5zZTU="
-                    },
-                    "forks": 22,
-                    "open_issues": 0,
-                    "watchers": 12,
-                    "default_branch": "master",
-                    "permissions": {
-                        "admin": False,
-                        "push": False,
-                        "pull": True
-                    }
-                },
-                {
-                    "id": 7776515,
-                    "node_id": "MDEwOlJlcG9zaXRvcnk3Nzc2NTE1",
-                    "name": "cpp-netlib",
-                    "full_name": "google/cpp-netlib",
-                    "license": {
-                        "key": "bsl-1.0",
-                        "name": "Boost Software License 1.0",
-                        "spdx_id": "BSL-1.0",
-                        "url": "https://api.github.com/licenses/bsl-1.0",
-                        "node_id": "MDc6TGljZW5zZTI4"
-                    },
-                    "forks": 59,
-                    "open_issues": 0,
-                    "watchers": 292,
-                    "default_branch": "master",
-                    "permissions": {
-                        "admin": False,
-                        "push": False,
-                        "pull": True
-                    }
-                }
-            ]
-        }
-        moked_get_json.return_value = test_payload["repos"]
         with patch(
                 "client.GithubOrgClient._public_repos_url",
                 new_callable=PropertyMock,
         ) as mocked_public_repos_url:
-            mocked_public_repos_url.return_value = test_payload["repos_url"]
+            mocked_public_repos_url.return_value = (
+                "https://api.github.com/orgs/google/repos"
+            )
             self.assertEqual(
                 GithubOrgClient("google").public_repos(),
                 [
-                    "episodes.dart",
-                    "cpp-netlib",
+                    "abc",
+                    "defg",
                 ],
             )
             mocked_public_repos_url.assert_called_once()
-        moked_get_json.assert_called_once()
+            moked_get_json.assert_called_once()
